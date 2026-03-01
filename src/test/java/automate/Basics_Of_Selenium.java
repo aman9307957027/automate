019edbd349bf67fcde733cfcbf5eccda97dca7d4
aman9307957027 <aman.j.deshmukhh@gmail.com>
2026-03-01 12:39:10 +0530

    First Program of selenium

---
+++ src/test/java/automate/Basics_Of_Selenium.java
@@ -0,0 +1,26 @@
+package automate;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Basics_Of_Selenium {
+
+	public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.google.com");
+	driver.manage().window().maximize();
+
+	}
+
+}
+package automate;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Basics_Of_Selenium {
+
+	public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.google.com");
+	driver.manage().window().maximize();
+
+	}
+
+}
