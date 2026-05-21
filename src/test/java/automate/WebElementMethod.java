4349a79b866e2e5059d6092bf7296d5cc32e8b94
aman9307957027 <aman.j.deshmukhh@gmail.com>
2026-05-21 18:01:34 +0530

    web element methods

--- src/test/java/automate/WebElementMethod.java
+++ src/test/java/automate/WebElementMethod.java
@@ -17,29 +17,49 @@ public class WebElementMethod {
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://omayo.blogspot.com/");
 		driver.manage().window().maximize();
+		// to get attribute value
 		WebElement textAreaField2=driver.findElement(By.cssSelector("textarea[cols='30']"));
 		String attributeValue=textAreaField2.getAttribute("rows");
 		System.out.println("Attribute value:- "+attributeValue);
+		
+		// to get display text is true or false
 		WebElement textDisplayedWithDelay=driver.findElement(By.cssSelector("#delayedText"));
 		System.out.println("Text Displayed:- "+textDisplayedWithDelay.isDisplayed());
 		Thread.sleep(10000);
 		System.out.println("Text Displayed:- "+textDisplayedWithDelay.isDisplayed());
+		
+		// to get css value
 		WebElement dropDownButton=driver.findElement(By.cssSelector(".dropbtn"));
 		String cssValue=dropDownButton.getCssValue("background-color");
 		System.out.println(cssValue);
+		
+		// to get screenshort of the element
 		File file=dropDownButton.getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(file,new File("src/test/resources/dropdown.png"));
+		
+		// to get size like height, width
 		org.openqa.selenium.Dimension d=dropDownButton.getSize();
 		System.out.println(d.getHeight());
 		System.out.println(d.getWidth());
+		
+		// to clear the textbox
 		textAreaField2.clear();
 		Thread.sleep(3000);
+		
+		// to enter input in textbox
 		textAreaField2.sendKeys("Welcome To codedisha");
+		
+		// to switch to alert popup and then accpet it
 		driver.findElement(By.cssSelector("#alert1")).click();
 		Thread.sleep(3000);
 		driver.switchTo().alert().accept();
+		
+		// to see text area or button is enable or disable and able to click or not
 		System.out.println("Is text area enabled:- "+textAreaField2.isEnabled());
+		
 		System.out.println("Is button enabled:- "+driver.findElement(By.cssSelector("#but1")).isEnabled());
+		
+		// to see the radio option or checkbox is selected or not
 		System.out.println("Bike radio button is selected:- "+driver.findElement(By.cssSelector("input[value='Bike']")).isSelected());
 		System.out.println("Bicycle radio button is selected:- "+driver.findElement(By.cssSelector("input[value='Bicycle']")).isSelected());
 		
